8071c91935492d5897ea4a66838f765d7ab00507
snake0 <1260865816@qq.com>
2021-10-08 14:15:08 +0800

    add GiantVM init

--- init.sh
+++ init.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-sudo apt update; sudo apt upgrade -y; sudo apt remove nano -y; sudo apt install -y build-essential git zsh ssh htop vim curl x11vnc net-tools unrar vlc tree wget qbittorrent qemu-system-x86
+sudo apt update; sudo apt upgrade -y; sudo apt remove nano -y; sudo apt install -y build-essential git zsh ssh htop vim curl x11vnc net-tools unrar vlc tree wget qbittorrent qemu-system-x86 build-essential openssl libncurses5-dev libssl-dev zlibc minizip libidn11-dev libidn11 bison flex python pkg-config libglib2.0-dev zlib1g-dev libpixman-1-dev libfdt-dev
 
-echo "export http_proxy=http://snake0:xyhqof-futbo5-bYfsyq@202.120.32.250:9081; expor https_proxy=http://snake0:xyhqof-futbo5-bYfsyq@202.120.32.250:9081;" >> ~/.bashrc
-source ~/.bashrc
+#echo "export http_proxy=http://snake0:xyhqof-futbo5-bYfsyq@202.120.32.250:9081; expor https_proxy=http://snake0:xyhqof-futbo5-bYfsyq@202.120.32.250:9081;" >> ~/.bashrc
+#source ~/.bashrc
 
-sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+#sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
